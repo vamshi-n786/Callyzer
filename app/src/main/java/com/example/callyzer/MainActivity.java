abab9df73a533bd36d861bfd4494b1b88187276d
vamshi N <nenavath_b190217cs@nitc.ac.in>
2022-03-21 22:25:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/callyzer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.callyzer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
